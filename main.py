e024f5e4c49bbab00224fdda8ef09492810fda49
Andrew Thielking <andrew@thielking.dev>
2025-10-18 08:26:07 -0400

    update for module 2

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/health', methods=['GET'])
+def handle_health():
+    return {"status": "Healthy"}, 200
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
