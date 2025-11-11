7ea5feb801b1d909aa54f70abecd0e22be1aade8
Andrew Thielking <andrew@thielking.dev>
2025-11-11 06:35:58 -0500

    update to helm

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/health', methods=['GET'])
 def handle_health():
-    return {"status": "Kustomize"}, 200
+    return {"status": "Helm!"}, 200
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
